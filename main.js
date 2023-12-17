0f5c30efa9e21d956a43f2a96f4aec7f2039d86c
Thom988 <t.quillet@hotmail.fr>
2023-12-17 23:41:01 +0100

    Affichage et developpement du solver termines

--- main.js
+++ main.js
@@ -1,74 +1,80 @@
 const sudoku = [
-  [0, 7, 9, 3, 2, 1, 6, 8, 0],
-  [5, 0, 0, 6, 4, 7, 0, 1, 9],
-  [1, 0, 0, 0, 8, 0, 7, 0, 4],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [7, 2, 0, 0, 9, 3, 8, 5, 1],
-  [8, 0, 0, 0, 0, 2, 4, 0, 0],
-  [0, 1, 0, 2, 0, 4, 5, 6, 0],
-  [6, 0, 0, 0, 5, 0, 3, 4, 0],
-  [0, 0, 4, 0, 3, 6, 0, 0, 7],
+  [0, 5, 0, 7, 8, 0, 0, 0, 0],
+  [0, 0, 8, 2, 3, 0, 7, 5, 6],
+  [2, 7, 4, 6, 1, 0, 0, 3, 9],
+  [0, 4, 0, 9, 0, 0, 0, 0, 0],
+  [0, 0, 0, 5, 0, 2, 0, 9, 8],
+  [0, 0, 2, 0, 0, 3, 1, 0, 7],
+  [0, 0, 0, 0, 0, 7, 0, 1, 0],
+  [4, 3, 0, 0, 0, 0, 9, 0, 5],
+  [1, 0, 9, 3, 0, 0, 0, 0, 0],
 ];
-// rappel : se lit (ligne,colonne);
+let tableau = [...sudoku];
 
-function resolve(sudoku) {
-  let tableau = Array.from(sudoku);
-  let regions = [];
-  for (let i = 0; i <= 6; i = i + 3) {
-    for (let j = 0; j <= 6; j = j + 3) {
-      regions.push(extraireRegion(tableau, i, j));
-    }
-  }
-  tableau = remplir(regions, tableau);
-}
-
-function remplir(regions, tableau) {
-
-}
-
-function testPrensenceNb(matrice, nb) {
-  let bool = false;
-  if (matrice[0][0] !== undefined) {
-
-    for (let i = 0; i < matrice.length; i++) {
-      for (let j = 0; j < matrice[i].length; j++) {
-        if (matrice[i][j] === nb) {
-          bool = true;
-        }
-      }
-    }
+function resoudre(tableau) {
+  let position = prochaineCaseVide(tableau);
+  if (position === null) {
+    return true;
   } else {
-    for (let i = 0; i < matrice.length; i++) {
-      if (matrice[i] === nb) {
-        bool = true;
+    for (let nb = 1; nb < 10; nb++) {
+      if (estValide(tableau, nb, position[0], position[1])) {
+        tableau[position[0]][position[1]] = nb;
+        if (resoudre(tableau)) {
+          return true;
+        }
+      } else {
+        tableau[position[0]][position[1]] = 0;
+      }
+    }
+    //si le sudoku n'est pas faisable
+    return false;
+  }
+}
+
+function estValide(tableau, nb, ligne, colonne) {
+  // Extraire et tester la ligne
+  const valeursLigne = tableau[ligne];
+  for (let i = 0; i < valeursLigne.length; i++) {
+    if (valeursLigne[i] === nb) {
+      return false;
+    }
+  } //Extraire et tester la colonne
+  const valeursColonne = [];
+  for (let i = 0; i < tableau.length; i++) {
+    valeursColonne.push(tableau[i][colonne]);
+  }
+  for (let j = 0; j < valeursColonne.length; j++) {
+    if (valeursColonne[j] === nb) {
+      return false;
+    }
+  } // Extraire et tester la région
+  ligneInit = Math.floor(ligne / 3) * 3;
+  colonneInit = Math.floor(colonne / 3) * 3;
+  for (let i = ligneInit; i < ligneInit + 3; i++) {
+    for (let j = colonneInit; j < colonneInit + 3; j++) {
+      if (tableau[i][j] === nb) {
+        return false;
       }
     }
   }
-  return bool;
+  return true;
 }
 
-function extraireRegion(tableau, ligneDepart, colonneDepart) {
-  let region = [];
-  for (let i = ligneDepart; i < ligneDepart + 3; i++) {
-    let ligne = [];
-    for (let j = colonneDepart; j < colonneDepart + 3; j++) {
-      ligne.push(tableau[i][j]);
-    }
-    region.push(ligne);
-  }
-  return region;
-}
-
-function extraireColonne(tableau, colonneDepart) {
-  const colonne = [];
+function prochaineCaseVide(tableau) {
   for (let i = 0; i < tableau.length; i++) {
-    colonne.push(tableau[i][colonneDepart]);
+    for (let j = 0; j < tableau[i].length; j++) {
+      if (tableau[i][j] === 0) {
+        return [i, j];
+      }
+    }
   }
-  return colonne;
+  return null;
 }
 
 function afficherTable(sudoku) {
-  const table = document.querySelector("#sudoku-table");
+  const tableElement = document.createElement("table");
+  const windowElement = document.querySelector(".sudoku-window");
+  tableElement.classList.add("sudoku-table");
   for (let i = 0; i < sudoku.length; i++) {
     const row = document.createElement("tr");
     row.dataset.id = `row-${i + 1}`;
@@ -83,10 +89,12 @@ function afficherTable(sudoku) {
       }
       row.appendChild(cellule);
     }
-
-    table.appendChild(row);
+    tableElement.appendChild(row);
   }
+  windowElement.appendChild(tableElement);
 }
 
-resolve(sudoku);
 afficherTable(sudoku);
+if (resoudre(tableau)) {
+    afficherTable(tableau);
+}
